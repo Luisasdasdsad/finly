a4b9a088f1bf31b3a49ca125ead319f089591fa5
luchito333 <76989444@continental.edu.pe>
2025-01-30 09:22:27 -0500

    Se creo el servidor y el routing

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const http = require('http');
+
+const PORT = 3000;
+
 const server = http.createServer((req, res) => {
-  console.log(req.url);
-  res.end('Hello From Node.js');
+  const { url } = req;
+  console.log(url);
+  if (url === '/') {
+    res.end('Hello From Node.js');
+  } else if (url === '/contact') {
+    res.end('The Contact Page');
+  } else if (url === '/about') {
+    res.end('The About Page');
+  } else {
+    res.writeHead(404);
+    res.end('Not Found');
+  }
 });
-server.listen(5500, () => {
-  console.log('Server running on port 3000');
+
+server.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
+  
